44fc8a155cebd74cc7fe0a0a8f4823eb9e5aed91
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2025-01-25 07:36:37 +0200

    improve the responce to show time and date and enviroment variable

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => {
+const port = process.env.PORT || 3000;
+app.get("/", async (req, res) => {
+  const dateTime = new Date();
+  const date = dateTime.getDate();
+  const month = dateTime.getMonth() + 1;
+  const year = dateTime.getFullYear();
+  const hour = dateTime.getHours() + 2;
+  const minutes = dateTime.getMinutes();
+  const seconds = dateTime.getSeconds();
   res.json({
-    content: "hi",
+    environment: process.env.NODE_ENV,
+    year,
+    month,
+    date,
+    hour,
+    minutes,
+    seconds,
   });
 });
-
 app.listen(port, () => {
   console.log(port);
 });
